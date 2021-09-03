916656b52fb350ac31cdd5282c8bf97dbc5a102a
Park Gyurim <guy021898@naver.com>
2021-09-04 03:22:03 +0900

    Increment #9
    - Add Create modal sheet in challengeListView
    - Catch Errors like parsing error from not matched data type in challengeListView
    
    Learned today :
    - [weak self] : Prevent strong reference cycle. When we use self within the closure, it's must better to use [weak self] in front of parameter, presenting weak reference. It's related to ARC (Auto Reference Count)

--- Increment/Increment/ViewModels/ChallengeListViewModel.swift
+++ Increment/Increment/ViewModels/ChallengeListViewModel.swift
@@ -9,11 +9,19 @@ import Foundation
 import Combine
 
 final class ChallengeListViewModel : ObservableObject {
+    let title = "Challenges"
     private let userService : UserServiceProtocol
     private let challengeService : ChallengeServiceProtocol
     private var cancellables : [AnyCancellable] = []
     @Published private(set) var itemViewModels : [ChallengeItemViewModel] = []
-    let title = "Challenges"
+    @Published private(set) var error : IncrementError?
+    @Published private(set) var isLoading : Bool = false
+    @Published var showingCreateModel : Bool = false
+    
+    enum Action {
+        case retry
+        case create
+    }
     
     init(userService : UserServiceProtocol = UserService(), challengeService : ChallengeServiceProtocol = ChallengeService()) {
         self.userService = userService
@@ -21,20 +29,40 @@ final class ChallengeListViewModel : ObservableObject {
         observeChallenges()
     }
     
+    func send(action : Action) {
+        switch action {
+        case .retry :
+            observeChallenges()
+        case .create :
+            showingCreateModel = true
+        }
+    }
+    
+    // check the challenges view
     private func observeChallenges() {
         // grab the current user id and pass it into a ChallengesService.observeChallenge
+        isLoading = true
+        
         userService.currentUser()
             .compactMap { $0?.uid }
-            .flatMap { userId -> AnyPublisher<[Challenge], IncrementError> in
+            .flatMap { [weak self] userId -> AnyPublisher<[Challenge], IncrementError> in
+                guard let self = self else { return Fail(error : .default()).eraseToAnyPublisher() }
                 return self.challengeService.observeChallenges(userId: userId)
-            }.sink { completion in
+            }.sink { [weak self] completion in
+                guard let self = self else { return }
+                self.isLoading = false
                 switch completion {
                 case let .failure(error) :
+                    self.error = error
                     print(error.localizedDescription)
                 case .finished :
                     print("finished")
                 }
-            } receiveValue: { challenges in
+            } receiveValue: { [weak self] challenges in
+                guard let self = self else { return }
+                self.isLoading = false
+                self.error = nil
+                self.showingCreateModel = false
                 print(challenges)
                 self.itemViewModels = challenges.map{ .init($0) }
             }.store(in : &cancellables)
---
+++ Increment/Increment/Views/ChallengeListView.swift
@@ -0,0 +1,100 @@
+//
+//  ChallengeListView.swift
+//  Increment
+//
+//  Created by Park Gyurim on 2021/08/31.
+//
+
+import SwiftUI
+
+struct ChallengeListView : View {
+    @StateObject private var viewModel : ChallengeListViewModel = ChallengeListViewModel()
+    
+    var body : some View {
+        ZStack {
+            if viewModel.isLoading {
+                ProgressView()
+            } else if let error = viewModel.error {
+                VStack {
+                    Text(error.localizedDescription)
+                    Button("Retry") {
+                        viewModel.send(action : .retry)
+                    }
+                    .padding()
+                    .background(Rectangle().fill(Color.red).cornerRadius(5))
+                }
+            } else {
+                mainContentView
+            }
+        }
+    }
+    
+    var mainContentView : some View {
+        ScrollView {
+            VStack {
+                LazyVGrid(columns: [.init(.flexible(), spacing : 20), .init(.flexible())], spacing : 20) {
+                    ForEach(viewModel.itemViewModels, id : \.self) { viewModel in
+                        challengeItemView(viewModel : viewModel)
+                    }
+                }
+                Spacer()
+            }
+        }
+        .sheet(isPresented : $viewModel.showingCreateModel) {
+            NavigationView {
+                CreateView()
+            }
+        }
+        .navigationBarItems(trailing : Button {
+            viewModel.send(action : .create)
+        } label: {
+            Image(systemName: "plus.circle")
+                .imageScale(.large)
+        })
+        .navigationTitle(viewModel.title)
+    }
+}
+
+struct challengeItemView : View {
+    private let viewModel : ChallengeItemViewModel
+    
+    init(viewModel : ChallengeItemViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var titleRow : some View {
+        HStack {
+            Text(viewModel.title)
+                .font(.system(size: 24, weight : .bold))
+            Spacer()
+            Image(systemName : "trash")
+        }
+    }
+    
+    var dailyIncreaseRow : some View {
+        HStack {
+            Text(viewModel.dailyIncreaseText)
+                .font(.system(size: 24, weight : .bold))
+            Spacer()
+        }
+    }
+    
+    var body : some View {
+        HStack {
+            Spacer()
+            VStack {
+                titleRow
+                Text(viewModel.statusText)
+                    .font(.system(size : 20, weight: .bold))
+                    .padding(25)
+                dailyIncreaseRow
+            }.padding(.vertical, 10)
+            Spacer()
+        }
+        .background(
+            Rectangle()
+                .fill(Color.primaryButton)
+                .cornerRadius(10)
+        )
+    }
+}
