e0649c381f0becda806091234247827fc2c7aa62
Park Gyurim <guy021898@naver.com>
2021-10-01 00:27:06 +0900

    Alamofire Multipart Request Test #2
    terminate due to server connection error.

--- MultipartApiTest/MultipartApiTest/ContentViewModel.swift
+++ MultipartApiTest/MultipartApiTest/ContentViewModel.swift
@@ -13,7 +13,7 @@ final class ContentViewModel : ObservableObject {
     private let url = "http://3.36.233.180:8080/used-posts"
     private let header : HTTPHeaders = [
         "Content-Type": "multipart/form-data",
-        "X-AUTH-TOKEN": "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGdtYWlsLmNvbSIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MzMwMTI0OTQsImV4cCI6MTYzMzAxNDI5NH0.s8BduFjUkBUyQeDqiJDANy5evHQyyi9XXGsGnhzTViU"
+        "X-AUTH-TOKEN": "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QGdtYWlsLmNvbSIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJpYXQiOjE2MzMwMTQzNzIsImV4cCI6MTYzMzAxNjE3Mn0.1UlC8zPuvo7HEEiUyTNMx3BIvphRb2ac6fsfyw1G_tc"
     ]
     
     private let files = UIImage(named: "testImg")!.pngData()! // Type : Data
@@ -26,13 +26,13 @@ final class ContentViewModel : ObservableObject {
             camps : [1, 2]
         )
     )
-    private let payload : [String : Any] = [
+    private let payload : [String : [String : String]] = [
         "postInfo" : [
             "title" : "iOS Test",
-            "price" : 1000,
+            "price" : "1000",
             "description" : "iOS Test",
             "category" : "iOS Test",
-            "camps" : [1, 2]
+            "camps" : "[1,2]"
         ]
     ]
     
@@ -45,35 +45,35 @@ final class ContentViewModel : ObservableObject {
     }
     
     func upload() { //(with payload : PostPayload)
-//        let encoder = JSONEncoder()
-//        let jsonData = try! encoder.encode(postPayload)
-//        let test = convertStringToDictionary(data: jsonData)
-//        print(test!)
-        
+        let encoder = JSONEncoder()
+        guard let jsonData = try? encoder.encode(payload) else {return}
+        //print(jsonData)
+        guard let jsonString = String(data : jsonData, encoding: .utf8) else { return }
+        print(jsonString)
+        let testString = "{\"postInfo\":{\"camps\":[1,2],\"title\":\"iOS Test\",\"description\":\"iOS Test\",\"category\":\"iOS Test\",\"price\":\"1000\"}}"
+
         AF.upload(multipartFormData: { [weak self] (multipartFormData) in
             guard let self = self else { return }
             
             // image
             multipartFormData.append(self.files, withName: "files", fileName : "testImg.png", mimeType: "image/png")
             // postInfo
-            for (key, value) in self.convertStringToDictionary(data: jsonData)! {
-                multipartFormData.append("\(value)".data(using: .utf8)!, withName: key, mimeType: "text/plain")
-            }
-            
-//            print(self.payload)
+//            for (key, value) in self.convertStringToDictionary(data: jsonData)! {
+//                multipartFormData.append("\(value)".data(using: .utf8)!, withName: key, mimeType: "text/plain")
+//            }
+//            multipartFormData.append(jsonString.data(using: .utf8)!, withName: "postInfo", mimeType: "application/json")
+            multipartFormData.append(testString.data(using: .utf8)!, withName: "postInfo", mimeType: "application/json")
+            print(testString)
 //            for (key, value) in self.payload {
-//                for (keyInsdie, valueInside) in value.self {
-//                    multipartFormData.append("\(valueInside)".data(using: .utf8)!,
-//                                             withName: keyInside,
-//                                             mimeType: "application/json"
-//                    )
-//                }
 //                print(key)
 //                print(value)
+//                for (keyInside, valueInsdie) in value {
+//                    print(keyInside + " " + valueInsdie)
+//                }
 //                multipartFormData.append("\(value)".data(using: .utf8)!, withName: key, mimeType: "application/json")
-            }
+//            }
             
-//            // postInfo
+//            // postInfo : separate
 //            multipartFormData.append("iOS TEST title".data(using: .utf8)!, withName: "postInfo[title]")
 //            multipartFormData.append("1000".data(using: .utf8)!, withName: "postInfo[price]")
 //            multipartFormData.append("iOS TEST desc".data(using: .utf8)!, withName: "postInfo[description]")
@@ -87,6 +87,7 @@ final class ContentViewModel : ObservableObject {
                     print("upload success : \(statusCode)")
                     print(response)
                 default :
+                    print(response.error ?? "")
                     print(response.description)
                     print("upload fail : \(statusCode)")
                 }
