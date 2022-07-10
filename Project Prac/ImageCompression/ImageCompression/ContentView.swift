0388d62a12a84babe2fe5ef954595933d3eef214
ParkGyurim99 <guy021898@naver.com>
2022-07-11 01:59:36 +0900

    Reduce Image Size with Loop #1
    📚 Ref) https://stackoverflow.com/questions/29137488/how-do-i-resize-the-uiimage-to-reduce-upload-image-size

--- Project Prac/ImageCompression/ImageCompression/ContentView.swift
+++ Project Prac/ImageCompression/ImageCompression/ContentView.swift
@@ -7,7 +7,13 @@
 
 import SwiftUI
 import PhotosUI
+
 extension UIImage {
+    func getSize() -> Int64? {
+        guard let imageData = jpegData(compressionQuality: 1.0) else { return nil }
+        return Int64(imageData.count) / 1048576
+    }
+    
     func getFileSizeInfo(allowedUnits: ByteCountFormatter.Units = .useMB,
                          countStyle: ByteCountFormatter.CountStyle = .memory,
                          compressionQuality: CGFloat = 1.0) -> String? {
@@ -20,6 +26,32 @@ extension UIImage {
     func getSizeInfo(formatter: ByteCountFormatter, compressionQuality: CGFloat = 1.0) -> String? {
         guard let imageData = jpegData(compressionQuality: compressionQuality) else { return nil }
         return formatter.string(fromByteCount: Int64(imageData.count))
+    }
+    
+    func resized(withPercentage percentage: CGFloat) -> UIImage? {
+        let canvasSize = CGSize(width: size.width * percentage, height: size.height * percentage)
+        UIGraphicsBeginImageContextWithOptions(canvasSize, false, scale)
+        defer { UIGraphicsEndImageContext() }
+        draw(in: CGRect(origin: .zero, size: canvasSize))
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+    
+    func resizedTo15MB() -> UIImage? {
+        guard let imageData = self.pngData() else { return nil }
+        
+        var resizingImage = self
+        var imageSizeKB = Double(imageData.count) / 1024
+        
+        while imageSizeKB > 15 * 1024 {
+            guard let resizedImage = resizingImage.resized(withPercentage: 0.5),
+                  let imageData = resizedImage.pngData()
+            else { return nil }
+            
+            resizingImage = resizedImage
+            imageSizeKB = Double(imageData.count) / 1024
+        }
+        
+        return resizingImage
     }
 }
 
@@ -61,13 +93,45 @@ struct ContentView: View {
                                     }
                                 )
                             Text(image.getFileSizeInfo() ?? "")
+                            Text("\(image.getSize() ?? 0)")
                         }
                     }
                 }.padding()
-            }.frame(height: UIScreen.main.bounds.width * 0.55)
+            }
             
             Divider()
-            
+
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(selectedPhotos, id : \.self) { image in
+                        if image.getSize()! > 15 {
+                            if let resized = image.resizedTo15MB() {
+                                VStack {
+                                    Image(uiImage: resized)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(width: UIScreen.main.bounds.width * 0.5,
+                                               height: UIScreen.main.bounds.width * 0.5)
+                                        .cornerRadius(16)
+                                    Text(resized.getFileSizeInfo() ?? "")
+                                    Text("\(resized.getSize() ?? 0)")
+                                }
+                            }
+                        } else {
+                            VStack {
+                                Image(uiImage: image)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: UIScreen.main.bounds.width * 0.5,
+                                           height: UIScreen.main.bounds.width * 0.5)
+                                    .cornerRadius(16)
+                                Text(image.getFileSizeInfo() ?? "")
+                                Text("\(image.getSize() ?? 0)")
+                            }
+                        }
+                    }
+                }.padding()
+            }
             
             Button {
                 showPhotosPicker.toggle()
@@ -81,8 +145,8 @@ struct ContentView: View {
             Spacer()
         }.sheet(isPresented: $showPhotosPicker) {
             PhotosPicker(configuration: configuration,
-                        isPresented: $showPhotosPicker,
-                        pickerResult: $selectedPhotos)
+                         isPresented: $showPhotosPicker,
+                         pickerResult: $selectedPhotos)
         }
     }
 }
