88e315b29dc7bf67d4d1b17c02c423375ff77765
ParkGyurim99 <guy021898@naver.com>
2022-07-10 21:38:28 +0900

    Get ImageSize
    📚 Ref) https://stackoverflow.com/questions/31314412/how-to-resize-image-in-swift

--- Project Prac/ImageCompression/ImageCompression/ContentView.swift
+++ Project Prac/ImageCompression/ImageCompression/ContentView.swift
@@ -7,6 +7,21 @@
 
 import SwiftUI
 import PhotosUI
+extension UIImage {
+    func getFileSizeInfo(allowedUnits: ByteCountFormatter.Units = .useMB,
+                         countStyle: ByteCountFormatter.CountStyle = .memory,
+                         compressionQuality: CGFloat = 1.0) -> String? {
+        let formatter = ByteCountFormatter()
+        formatter.allowedUnits = allowedUnits
+        formatter.countStyle = countStyle
+        return getSizeInfo(formatter: formatter, compressionQuality: compressionQuality)
+    }
+    
+    func getSizeInfo(formatter: ByteCountFormatter, compressionQuality: CGFloat = 1.0) -> String? {
+        guard let imageData = jpegData(compressionQuality: compressionQuality) else { return nil }
+        return formatter.string(fromByteCount: Int64(imageData.count))
+    }
+}
 
 struct ContentView: View {
     @State private var showPhotosPicker : Bool = false
@@ -25,25 +40,28 @@ struct ContentView: View {
             ScrollView(.horizontal) {
                 HStack {
                     ForEach(selectedPhotos, id : \.self) { image in
-                        Image(uiImage: image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: UIScreen.main.bounds.width * 0.5,
-                                   height: UIScreen.main.bounds.width * 0.5)
-                            .cornerRadius(16)
-                            .overlay(
-                                Button {
-                                    if let index = selectedPhotos.firstIndex(of: image) {
-                                        selectedPhotos.remove(at: index)
+                        VStack {
+                            Image(uiImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: UIScreen.main.bounds.width * 0.5,
+                                       height: UIScreen.main.bounds.width * 0.5)
+                                .cornerRadius(16)
+                                .overlay(
+                                    Button {
+                                        if let index = selectedPhotos.firstIndex(of: image) {
+                                            selectedPhotos.remove(at: index)
+                                        }
+                                    } label : {
+                                        Image(systemName : "xmark")
+                                            .foregroundColor(.black)
+                                            .padding(8)
+                                            .background(Color.gray.opacity(0.5))
+                                            .clipShape(Circle())
                                     }
-                                } label : {
-                                    Image(systemName : "xmark")
-                                        .foregroundColor(.black)
-                                        .padding(8)
-                                        .background(Color.gray.opacity(0.5))
-                                        .clipShape(Circle())
-                                }
-                            )
+                                )
+                            Text(image.getFileSizeInfo() ?? "")
+                        }
                     }
                 }.padding()
             }.frame(height: UIScreen.main.bounds.width * 0.55)
