8877e60dd1070acfb423e10cf0e1ad4507446ca8
ParkGyurim99 <guy021898@naver.com>
2023-03-12 22:39:00 +0900

    AdvancedScrollView

---
+++ Project Prac/AdvancedScrollView/AdvancedScrollView/AdvancedScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedScrollViewApp.swift
+//  AdvancedScrollView
+//
+//  Created by Park Gyurim on 2023/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
